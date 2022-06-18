0b71e456844de665e8f9a1446289a9d5706c60f9
Burakhan Bircan <burakhan.bircan@lunatr.com>
2022-06-18 11:35:30 +0300

    IdentityServer Config dosyası düzenlendi. GetUser endpointi eklendi.

--- UdemyMicroservices/IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
+++ UdemyMicroservices/IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using FreeCourse.Shared.Dtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 using static IdentityServer4.IdentityServerConstants;
@@ -33,13 +34,27 @@ namespace FreeCourse.IdentityServer.Controllers
                 City = signupDto.City,
             };
 
-            var result = await _userManager.CreateAsync(user,signupDto.Password);
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
             if (!result.Succeeded)
             {
-                return BadRequest(Response<Shared.Dtos.NoContent>.Fail(result.Errors.Select(x=>x.Description).ToList(),400));
+                return BadRequest(Response<Shared.Dtos.NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
             }
 
-            return NoContent(); 
+            return NoContent();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            var userIdClaim = User.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Sub);
+
+            if (userIdClaim == null) return BadRequest();
+
+            var user = await _userManager.FindByIdAsync(userIdClaim.Value);
+
+            if (user == null) return BadRequest();
+
+            return Ok(new { Id = user.Id, UserName = user.UserName, Email = user.Email, City = user.City });
         }
     }
 }
